d23a2ce4aebdfced340fc08d5bbc4690fb1dc298
EruditeCode <erudite.home@gmail.com>
2023-04-21 21:46:58 +0100

    Updating the get_overlap_groups to prevent skipping overlaps.

--- rocket_engine_basic/support_functions.py
+++ rocket_engine_basic/support_functions.py
@@ -6,19 +6,25 @@ def barriers_to_walls(barriers):
 		for i in range(len(barrier)-1,-1,-1):
 			walls.append([barrier[i], barrier[i-1]])
 	return walls
-
+	
 def get_overlap_groups(particles, axis=0):
+	# Tidy this up!!!
 	groups = []
-	group = [particles[0]] 
+	group = [particles[0]]
+	benchmark = particles[0].pos[axis] + particles[0].rad
 	for i in range(1, len(particles)):
-		if particles[i].pos[axis] <= group[0].pos[axis]+group[0].rad+particles[i].rad:
+		if particles[i].pos[axis]-particles[i].rad <= benchmark:
 			group.append(particles[i])
+			if particles[i].pos[axis] + particles[i].rad > benchmark:
+				benchmark = particles[i].pos[axis] + particles[i].rad
 		else:
 			groups.append(group.copy())
 			group = [particles[i]]
+			benchmark = particles[i].pos[axis] + particles[i].rad
 	groups.append(group)
 	return groups
 
+# Need to improve this to add an actual collision check...
 def sweep_and_prune(particles):
 	# Sort by x-axis.
 	x_sorted = get_overlap_groups(particles)
