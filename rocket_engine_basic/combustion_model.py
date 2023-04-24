f51f67c507708482ecb6639b17e9468fd566b676
EruditeCode <erudite.home@gmail.com>
2023-04-24 21:26:25 +0100

    Test model now fully working with reaction between oxidizer and fuel.

--- rocket_engine_basic/combustion_model.py
+++ rocket_engine_basic/combustion_model.py
@@ -21,15 +21,15 @@ def main():
 	bg = pg.Surface((WIDTH, HEIGHT))
 	bg.fill((20,20,20))
 
-	particles = [Fuel((100, 300), (2.9, -3.1), 40), Fuel((500, 400), (-2.5, 4.1), 40),
-				Fuel((40, 50), (1.9, 2.1), 40), Fuel((550, 50), (-1.5, -4.1), 40),
-				Fuel((80, 500), (4.9, -0.6), 40), Fuel((600, 200), (2.5, 3.1), 40),
+	particles = [Fuel((100, 300), (2.9, -3.1), 40), Ox((500, 400), (-2.5, 4.1), 40),
+				Fuel((45, 50), (1.9, 2.1), 40), Ox((550, 50), (-1.5, -4.1), 40),
+				Fuel((80, 500), (4.9, -0.6), 40), Ox((600, 200), (2.5, 3.1), 40),
 				]
 
-	# particles = [Fuel((100, 300), (0, 0), 40), Fuel((130, 310), (0, 0), 40)]
+	# particles = [Fuel((100, 300), (0.001,-1), 40)]
 
 	# Flags and Settings
-	dt = 0.5
+	dt = 2
 	simulate = False
 	while True:
 		for event in pg.event.get():
@@ -45,10 +45,8 @@ def main():
 			particles.sort(key=lambda x: x.pos[0], reverse=False)
 			collision_set = sweep_and_prune(particles)
 			deletable, new = [], []
-			# print(collision_set)
 			for collision_group in collision_set:
 				while len(collision_group) > 1:
-					print(collision_group)
 					collision_group, products, delete = collision_group[0].update_particle_collision(collision_group)
 					deletable.extend(delete)
 					new.extend(products)
@@ -72,8 +70,7 @@ def main():
 				x_min, x_max = min([wall[0][0], wall[1][0]]), max([wall[0][0], wall[1][0]])
 				y_min, y_max = min([wall[0][1], wall[1][1]]), max([wall[0][1], wall[1][1]])
 				for p in particles_close_to_walls:
-					if ((x_min-p.rad<=p.pos[0]<=x_max+p.rad) and (y_min-p.rad<=p.pos[1]<=y_max+p.rad)):
-						# At present the update wall doesn't perform continuous calc...
+					if ((x_min-p.rad*2<=p.pos[0]<=x_max+p.rad*2) and (y_min-p.rad*2<=p.pos[1]<=y_max+p.rad*2)):
 						p.update_wall_collision(wall, dt)
 
 			# Update particle positions.
